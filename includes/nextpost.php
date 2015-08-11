de0af08177254f65772b1942ad05ce444fc1142d
Joseph Benson <joseph.benson@alumni.usc.edu>
2015-08-11 09:03:32 -0700

    Adds ACF repeater in Options to filter out taxonomies from the post pagination links
    -Add one or more taxonomy slugs to the "Adjacent post excluded taxonomies" list to block posts in those taxnomies from being included in the Next/Previous post links at the bottom of post pages
    -Use the special taxonomies "post-format-<name>" to block a post format, e.g. "post-format-quote" to prevent quotes from being included

--- includes/nextpost.php
+++ includes/nextpost.php
@@ -1,4 +1,16 @@
-  <div class="w-section w-clearfix older-newer-posts">  		
-  		<div class="button older-button"><?php next_post_link('%link','Previous Story','false','31'); ?></div>
-	  	<div class="button newer-button"><?php previous_post_link('%link','Next Story','false','31'); ?></div>
-  </div>
+<?php
+if ( have_rows( 'adjacent_post_excluded_taxonomies', 'option' ) ) {
+    $excludedTaxIds = array();
+    while ( have_rows( 'adjacent_post_excluded_taxonomies', 'option' ) ) {
+        the_row();
+        $slug = get_sub_field( 'slug' );
+        if ( $taxObj = get_term_by( 'slug', $slug, 'post_format' ) ) {
+            $excludedTaxIds[] = (int) $taxObj->term_id;
+        }
+    }
+}
+?>
+<div class="w-section w-clearfix older-newer-posts">
+    <div class="button older-button"><?php next_post_link( '%link', 'Previous Story', false, $excludedTaxIds ); ?></div>
+    <div class="button newer-button"><?php previous_post_link( '%link', 'Next Story', false, $excludedTaxIds ); ?></div>
+</div>
